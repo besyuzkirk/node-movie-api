3f694ef8d5297b3ca35f21577d70ef6d6d07735d
Ahmet Karademir <ahmet.karademir@boun.edu.tr>
2022-08-21 21:59:29 +0300

    NMA-1 changedErrorMessages

--- helper/db.js
+++ helper/db.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 module.exports = () => {
     mongoose.connect('mongodb+srv://besyuzkirk:z9mPgruWxPTdPCT@movie-db.tvs7i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
     mongoose.connection.on('open',() => {
-        console.log("açık")
+        console.log("connection is open")
     })
     mongoose.connection.on('error',(err) => {
-        console.log("kapalı", err)
+        console.log("connection is close", err)
     })
     mongoose.Promise = global.Promise;
 };
--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
+	
 	const token = req.headers['x-access-token'] || req.body.token || req.query.token
 
 	if(token){
